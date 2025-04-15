e763f7739093b76e6fe336b304af2042863ae178
Nirupom Debnath <134253252+Nirupom-debnath@users.noreply.github.com>
2025-04-15 17:08:33 +0600

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,5 +2,32 @@ import User from "../models/User.js";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 
+const registerUser = async (req, res) => {
+  try {
+    const { username, email, password, photo } = req.body;
+    // Check if the email is already registered
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email is already registered" });
+    }
 
+    // Hash the password
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    // Create a new user
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+      photo,
+    });
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
