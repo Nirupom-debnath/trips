fc3fd1c95f5e692fb8fd15a996e2818b00e4ed03
Nirupom Debnath <134253252+Nirupom-debnath@users.noreply.github.com>
2025-04-15 17:10:35 +0600

    update

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -53,3 +53,56 @@ const createBooking = async (req, res) => {
 };
 
 // Get a specific booking by ID
+const getBooking = async (req, res) => {
+  try {
+    const bookingId = req.params.id;
+    const booking = await Booking.find({ userId: bookingId }).sort({
+      timestamps: 1,
+    });
+
+    if (!booking) {
+      return res.status(404).json({ message: "Booking not found" });
+    }
+
+    res.status(200).json({ success: true, data: booking });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+// Get all bookings
+const getAllBookings = async (req, res) => {
+  try {
+    const bookings = await Booking.find().sort({
+      updatedAt: -1,
+    });
+    res
+      .status(200)
+      .json({ success: true, data: bookings, count: bookings.length });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+// Delete a booking by ID
+const deleteBooking = async (req, res) => {
+  try {
+    const bookingId = req.params.id;
+    const deletedBooking = await Booking.findByIdAndDelete(bookingId);
+
+    if (!deletedBooking) {
+      return res.status(404).json({ message: "Booking not found" });
+    }
+
+    res
+      .status(200)
+      .json({ success: true, message: "Booking deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export { createBooking, getBooking, getAllBookings, deleteBooking };
